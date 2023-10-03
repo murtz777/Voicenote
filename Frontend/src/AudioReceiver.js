35c2c510840b82ea9ec34c34616cbd63e8229ea0
murtz777 <info@vcaretechnologies.net>
2023-10-03 17:24:30 +0530

    Update AudioReceiver.js

--- Frontend/src/AudioReceiver.js
+++ Frontend/src/AudioReceiver.js
@@ -4,52 +4,38 @@ import AudioPlayer from "react-h5-audio-player";
 import "react-h5-audio-player/lib/styles.css";
 import axios from "axios";
 
-const socket = io(
-  // ("https://api.vcaretechnologies.net");
-  "http://localhost:5000"
-); // Replace with your server URL
+const socket = io("https://api.vcaretechnologies.net");
+// ("http://localhost:5000"); // Replace with your server URL
 
 const AudioReceiver = ({ selectedGroup }) => {
   const [audioMessages, setAudioMessages] = useState([]);
-  const [currentGroup, setCurrentGroup] = useState(""); // State to track the current group
+  const [currentGroup, setCurrentGroup] = useState("");
   const [loading, setLoading] = useState(true);
+  const [refreshInterval, setRefreshInterval] = useState(null);
   const [lastAudioIndex, setLastAudioIndex] = useState(-1);
   const [textMessages, setTextMessages] = useState([]);
   const [imageMessages, setImageMessages] = useState([]);
 
-
   const audioPlayerRef = useRef(null);
 
-  // const fetchAudioMessages = async () => {
-  //   try {
-  //     const response = await axios.get(
-  //       `http://localhost:5000/api/audio-messages?group=${selectedGroup}`
-  //     );
-  //     setAudioMessages(response.data);
-  //     console.log("response===>", response.data);
-  //     setLoading(false);
-  //   } catch (error) {
-  //     console.log("error===>", error);
-  //   }
-  // };
-  const fetchMessages = async () => {
+  const fetchAudioMessages = async () => {
     try {
       const response = await axios.get(
-        `http://localhost:5000/api/messages?group=${selectedGroup}`
+        `https://api.vcaretechnologies.net/api/audio-messages?group=${selectedGroup}`
       );
-      setAudioMessages(response.data.audioMessages);
-      setTextMessages(response.data.textMessages);
-      setImageMessages(response.data.imageMessages);
-      console.log("Messages response ===>", response.data);
+      setAudioMessages(response.data);
+      setTextMessages(response?.data?.textMessages);
+      setImageMessages(response?.data?.imageMessages);
+      console.log("res====>", response);
       setLoading(false);
     } catch (error) {
-      console.error("Error fetching messages:", error);
+      console.log("error===>", error);
     }
   };
+  console.log("audioMessages===--->", audioMessages);
 
   useEffect(() => {
-    // fetchAudioMessages();
-    fetchMessages();
+    fetchAudioMessages();
     if (selectedGroup !== currentGroup) {
       setCurrentGroup(selectedGroup);
 
@@ -58,6 +44,11 @@ const AudioReceiver = ({ selectedGroup }) => {
       }
     }
     socket.on("received-audio", (audioPath) => {
+      //   setAudioMessages((prevMessages) => [
+      //     ...prevMessages,
+      //     { audio_url: audioPath },
+      //   ]);
+      // });
       const newAudioMessage = {
         audio_url: audioPath,
         group_name: selectedGroup,
@@ -74,18 +65,57 @@ const AudioReceiver = ({ selectedGroup }) => {
       setImageMessages((prevImages) => [...prevImages, imageUrl]);
     });
 
-    setCurrentGroup(selectedGroup);
-
     socket.on("received-text", (textMessage) => {
       setTextMessages((prevMessages) => [...prevMessages, textMessage]);
     });
+    setCurrentGroup(selectedGroup);
+
     return () => {
       socket.off("received-audio");
       socket.off("received-text");
       socket.off("received-image");
     };
   }, [selectedGroup, currentGroup]);
+  useEffect(() => {
+    if (refreshInterval) {
+      clearInterval(refreshInterval); // Clear the previous interval
+    }
 
+    // Start a new interval that fetches audio messages every 2 seconds
+    const newInterval = setInterval(fetchAudioMessages, 1000);
+
+    setRefreshInterval(newInterval); // Store the new interval in state
+
+    return () => {
+      if (newInterval) {
+        clearInterval(newInterval); // Clear the interval when the component unmounts
+      }
+    };
+  }, [selectedGroup]);
+
+  //   return (
+  //     <div>
+  //       {audioMessages.map((message, index) => (
+
+  //         <div key={index}>
+  //           {console.log("Audio src:", `/audio/${message.audio_url}`)}
+  //           <AudioPlayer src={`/audio/${message.audio_url}`} autoPlay controls />
+  //         </div>
+
+  //       ))}
+  //     </div>
+
+  //   );
+  // };
+
+  // // Function to handle group change
+  // const handleGroupChange = (group) => {
+  //   // setSelectedGroup(group);
+  //   // Clear the audio messages when changing groups
+  //   setAudioMessages([]);
+  //   console.log("Selected Group:", group);
+  //   socket.emit("join-group", group);
+  // };
   return (
     <div>
       <p>Selected Group: {selectedGroup}</p>
@@ -98,19 +128,19 @@ const AudioReceiver = ({ selectedGroup }) => {
       ))}
 
       {audioMessages.map((message, index) => {
-        const audioUrl = `http://localhost:5000${message.audio_url}`;
+        const audioUrl = `https://api.vcaretechnologies.net${message.audio_url}`;
 
+        console.log("Audio URL====:", audioUrl);
         if (message.group_name === selectedGroup) {
           const isLastAudio = index === lastAudioIndex;
-
           return (
             <div key={index}>
               <AudioPlayer
                 ref={audioPlayerRef}
                 src={audioUrl}
                 controls
-                autoPlay={isLastAudio}
                 autoPlayAfterSrcChange={false}
+                autoPlay={isLastAudio}
               />
             </div>
           );
@@ -118,30 +148,16 @@ const AudioReceiver = ({ selectedGroup }) => {
         return null;
       })}
 
-      {/* {imageMessages?.map((message, index) => {
-        const imageUrl = `http://localhost:5000${message.image_url}`;
-
-        if (message.group_name === selectedGroup) {
-          return (
-            <div key={index}>
-              <img src={imageUrl} alt={`Image ${index}`} />
-            </div>
-          );
-        }
-        return null;
-      })} */}
-      {/* Display received images */}
-
       {imageMessages?.map((imageUrl, index) => (
         <div key={index}>
           {console.log(`Image URL ${index}:`, imageUrl.image_url)}
           <img
-            src={`http://localhost:5000${imageUrl.image_url}`}
+            src={`https://api.vcaretechnologies.net${imageUrl.image_url}`}
             alt={`Image ${index}`}
             style={{
               maxWidth: "200px", // Adjust the maximum width as needed
               maxHeight: "200px", // Adjust the maximum height as needed
-              marginBottom:"10px",
+              marginBottom: "10px",
               display: "block",
             }}
           />
